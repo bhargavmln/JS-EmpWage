a8a99fd58351ba62c7441f4c90eaba8ac937ebe3
bhargavmln <bhargavmln@gmail.com>
2020-11-11 09:00:19 +0530

    [bhargavmln]add:ability to calculate emp wage

--- NewEmployeeWage.js
+++ NewEmployeeWage.js
@@ -9,26 +9,30 @@
     }
 }
 
-//UC2 Refactored -> UC3
+//UC4
 {
     const IS_PART_TIME = 1;
     const IS_FULL_TIME = 2;
     const PART_TIME_HOURS = 4;
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOUR = 20;
+    const TOTAL_WORKING_DAYS = 20;
 
     function getWorkingHours(checkForPresenceOrAbsence){
     switch(checkForPresenceOrAbsence){
         case IS_PART_TIME : 
            return PART_TIME_HOURS;
         case IS_FULL_TIME : 
-           return FULL_TIME_HOURS
+           return FULL_TIME_HOURS;
         default : 
            return 0;
        }
     }
-       let checkForPresenceOrAbsence =  Math.floor(Math.random()*10) % 3 ;
-       let empHrs = getWorkingHours(checkForPresenceOrAbsence);
-       let empWage = empHrs * WAGE_PER_HOUR;
-       console.log("Hours Worked by Employee: "+empHrs+" Employee Wage : "+empWage);
-    }
+       let totalEmpHrs = 0;
+       for(let day = 0; day<TOTAL_WORKING_DAYS; day++){
+        let checkForPresenceOrAbsence =  Math.floor(Math.random()*10) % 3 ;
+        totalEmpHrs += getWorkingHours(checkForPresenceOrAbsence)
+       }
+       let empWage = totalEmpHrs * WAGE_PER_HOUR;
+       console.log("Hours Worked by Employee: "+ totalEmpHrs +" Employee Wage : "+empWage);
+}
