727227dbb5c25ac4a48e19d0260164b26eaf04f9
bhargavmln <bhargavmln@gmail.com>
2020-11-13 17:16:01 +0530

    [bhargavmln]add:ability to do object operations using arrow

--- NewEmployeeWage.js
+++ NewEmployeeWage.js
@@ -28,23 +28,46 @@ function getWorkingHours(checkForPresenceOrAbsence) {
 }
 
 //UC10 Objects
-{
-    let empDailyHrsandWageArr = new Array();
-    while (totalEmpHrs <= MAX_HOURS_IN_A_MONTH && totalWorkingDays < TOTAL_WORKING_DAYS) {
-        totalWorkingDays++;
-        let empCheck = Math.floor(Math.random() * 10) % 3;
-        let empHrs = getWorkingHours(empCheck);
-        totalEmpHrs += empHrs;
-        empDailyHrsandWageArr.push(
-            {
-                dayNum: totalWorkingDays,
-                dailyHours: empHrs,
-                dailyWage: calcDailyWage(empHrs),
-                toString() {
-                    return '\nDay' + this.dayNum + ' => Working Hours is ' + this.dailyHours +
-                        ' And Wage earned = ' + this.dailyWage
-                },
-            });
-    }
-    console.log(`UC10 Showing Daily Hours Worked and Wage Earned: ${empDailyHrsandWageArr}`);
+let empDailyHrsandWageArr = new Array();
+while (totalEmpHrs <= MAX_HOURS_IN_A_MONTH && totalWorkingDays < TOTAL_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyHrsandWageArr.push(
+        {
+            dayNum: totalWorkingDays,
+            dailyHours: empHrs,
+            dailyWage: calcDailyWage(empHrs),
+            toString() {
+                return '\nDay' + this.dayNum + ' => Working Hours is ' + this.dailyHours +
+                    ' And Wage earned = ' + this.dailyWage
+            },
+        });
 }
+console.log(`\n\nUC10 Showing Daily Hours Worked and Wage Earned: ${empDailyHrsandWageArr}`);
+
+//UC11A - 11D
+let totalWages = empDailyHrsandWageArr
+    .filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
+    .reduce((totalWage, dailyHrsAndWage) => totalWage += dailyHrsAndWage.dailyWage, 0);
+let totalHours = empDailyHrsandWageArr
+    .filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
+    .reduce((totalHours, dailyHrsAndWage) => totalHours += dailyHrsAndWage.dailyHours, 0);
+console.log("\n\nUC 11A Total Hours: " + totalHours + " Total Wages: "+ totalWages);
+
+process.stdout.write("\n\nUC 11B Logging Full Work Days")
+empDailyHrsandWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 8)
+    .forEach(dailyHrsAndWage => process.stdout.write(dailyHrsAndWage.toString()));
+
+
+let partWorkingDaysStrArr =empDailyHrsandWageArr
+    .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 4)
+    .map(dailyHrsAndWage => dailyHrsAndWage.toString());
+process.stdout.write("\n\nUC 11C  Part Work Days: " + partWorkingDaysStrArr);
+
+let nonWorkingDaysStrArr =empDailyHrsandWageArr
+    .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 0)
+    .map(dailyHrsAndWage => dailyHrsAndWage.toString());
+process.stdout.write("\n\nUC 11C No Work Days: " + nonWorkingDaysStrArr);
+
